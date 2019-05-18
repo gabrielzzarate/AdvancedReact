60045900811bb78f29fb1d1f14d77aa14d292c04
Gabriel Zarate <gabrielzzarate@gmail.com>
2019-05-18 07:40:35 -0400

    Items type, query, and mutation

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,19 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+/* 
+if the query matches whats on the server exactly, and you don't need to do any other operations like authentication, etc, you can just forward the query on to the db prisma instance.
+*/
+
+const Query = {
+  items: forwardTo('db'),
+};
+
+// const Query = {
+//   async items(parent, args, ctx, info) {
+//     const items = ctx.db.query.items();
+
+//     return items;
+//   }
+// };
 
 module.exports = Query;
