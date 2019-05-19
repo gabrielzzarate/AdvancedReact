851607c747e05cec253af0feb8ea45c36b8e37a3
Gabriel Zarate <gabrielzzarate@gmail.com>
2019-05-19 06:29:09 -0400

    begin items page

---
+++ sick-fits/frontend/components/Items.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
+`;
+
+export default class Items extends Component {
+  render() {
+    return (
+      <div>
+        <p>Items</p>
+        <Query query={ALL_ITEMS_QUERY}>
+          {({ data, error, loading }) => {
+            console.log(payload);
+            return <p>Hey I'm the child of query</p>
+          }}
+        </Query>
+      </div>
+    )
+  }
+}
